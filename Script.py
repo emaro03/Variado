40b692edf51933c1bc6a39535d7b85a5a40038b2
Emanuel Almonte R <emaro03@gmail.com>
2020-07-27 22:41:18 -0400

    Envía correo electrónico a clientes y adjunto

--- Script.py
+++ Script.py
@@ -4,26 +4,27 @@ from email.mime.multipart import MIMEMultipart
 from email.mime.base import MIMEBase
 from email import encoders
 import os
+import numpy as np
 
-email_user = 'ealmonter@ennova.com.do'
-email_password = '70o27Rfnq01f'
+email_user = 'emaro03@gmail.com'
+email_password = 'ikjglmdnfjbcffuj'
 
-archivos = {'ejemplo1.txt': 'emaro03@gmail.com', 'ejemplo2.txt': 'emanuel.almonte@hotmail.com'}
+archivos = {r'C:\Users\Owner\Documents\GitHub\ENNOVA\Archivos de ejemplo\ejemplo1.txt': 'emaro03@gmail.com', r'C:\Users\Owner\Documents\GitHub\ENNOVA\Archivos de ejemplo\ejemplo2.txt': 'emaro03@gmail.com'}
 
-for usuario, mails in archivos.items():
+for archivo, mails in archivos.items():
     email_send = (mails)
     msg = MIMEMultipart()
     msg['From'] = email_user
     msg['To'] = ",".join(email_send)
-    msg['Subject'] = 'Factura eléctrica'
+    msg['Subject'] = 'Correo electrónico de prueba'
 
-    body = 'Estimado cliente, el siguiente correo es para hacerle llegar su factura eléctrica'
+    body = 'Este sería el cuerpo del correo'
     msg.attach(MIMEText(body,'html'))
 
     part = MIMEBase('application','octet-stream')
-    part.set_payload((open(usuario,'rb')).read())
+    part.set_payload((open(archivo,'rb')).read())
     encoders.encode_base64(part)
-    part.add_header('Content-Disposition', 'attachment; filename="{0}"'.format(os.path.basename(usuario)))
+    part.add_header('Content-Disposition', 'attachment; filename="{0}"'.format(os.path.basename(archivo)))
     msg.attach(part)
 
     mail = smtplib.SMTP('smtp.gmail.com',587)
